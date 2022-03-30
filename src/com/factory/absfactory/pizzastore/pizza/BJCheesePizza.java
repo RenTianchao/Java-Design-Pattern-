2495d596a3efddc7721939ff6432d13044d9a0f8
RenTianchao <1041348798@qq.com>
2022-03-30 21:53:59 +0800

    update code

---
+++ src/com/factory/absfactory/pizzastore/pizza/BJCheesePizza.java
@@ -0,0 +1,27 @@
+package com.factory.absfactory.pizzastore.pizza;
+
+public class BJCheesePizza extends Pizza{
+    @Override
+    public void prepare() {
+        super.prepare();
+        System.out.println("给BJPepperPizza准备原材料");
+    }
+
+    @Override
+    public void bake() {
+        super.bake();
+        System.out.println("给BJPepperPizza烘焙");
+    }
+
+    @Override
+    public void cut() {
+        super.cut();
+        System.out.println("给BJPepperPizza切割");
+    }
+
+    @Override
+    public void box() {
+        super.box();
+        System.out.println("给BJPepperPizza装盒");
+    }
+}
