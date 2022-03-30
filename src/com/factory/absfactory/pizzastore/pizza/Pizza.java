6c376dd9f1c80d83b6a536c29950ae9741c45204
RenTianchao <1041348798@qq.com>
2022-03-30 21:53:59 +0800

    10.0

---
+++ src/com/factory/absfactory/pizzastore/pizza/Pizza.java
@@ -0,0 +1,25 @@
+package com.factory.absfactory.pizzastore.pizza;
+
+public class Pizza {
+    String name;
+    public void prepare(){
+
+    }
+    public  void bake(){
+
+    }
+    public  void cut(){
+
+    }
+    public void box(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
